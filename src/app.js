55a59118430f73ef9dba5a1ee716ad05c9431271
Balim1 <jpmontoya70@gmail.com>
2023-03-04 01:50:11 +0000

    comment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  const symbols = console.log("Hello Rigo from the console!");
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function getRandomNumbers(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  let cards = document.querySelector(".card");
+  let symbol1 = document.querySelector(".symbol1");
+  let numbers = document.querySelector(".number");
+  let symbol2 = document.querySelector(".symbol2");
+
+  cards.classList.add(symbols[getRandomNumbers(symbols)]);
+  cards.classList.add(number[getRandomNumbers(number)]);
+  symbol1.innerHTML = symbols[getRandomNumbers(symbols)];
+  numbers.innerHTML = number[getRandomNumbers(number)];
+  symbol2.innerHTML = symbols[getRandomNumbers(symbols)];
 };
